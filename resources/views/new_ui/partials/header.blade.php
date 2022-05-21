2292ac492eaaf4ea37db3c4e28eb4548e8c9c310
mfajririki <fajririki07@gmail.com>
2022-05-21 11:28:57 +0700

    new ui

---
+++ resources/views/new_ui/partials/header.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row mx-auto" style="margin-top: 100px">
+        <div class="col align-self-center">
+            <p class="judul">PROGRAM STUDI PENDIDIKAN OLAHRAGA</p>
+            <P class="judul">FAKULTAS KEGURUAN, ILMU PENDIDIKAN & BAHASA</P>
+            <P class="judul">UNIVERSITAS BINA DARMA</P>
+            <img src={{ asset('assets/images/PendidikanOlahraga.png') }} alt="Pendidikan Olahraga Akreditasi"
+                style="width: 200px" class="text-end">
+        </div>
+        <div class="col text-end">
+            <img src={{ asset('assets/images/Frame_8.png') }} alt="www.binadarma.ac.id" style="width: 450px">
+        </div>
+    </div>
+</div>
